0d2cc05d13074334b7265ba5c80d5cdfa7f6157f
IvicaPizent <ivica.pizent@gmail.com>
2020-08-05 14:22:34 +0200

    Deploy

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'b^=%qm^rsp&kqf(@cf!h&3eyru26aa1yt+6%erg(14ejy5sw6r'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'beecheck.herokuapp.com',
+    #'beecheck.herokuapp.com',
 ]
 
 INSTALLED_APPS = [
